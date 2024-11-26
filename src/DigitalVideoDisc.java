91e06eaa93f5aa3b862b986769e99312608232dd
nguyendinhluong20225878 <luong09876789@gmail.com>
2024-11-26 21:50:59 +0700

    TestDigitalVideoDisc

--- src/DigitalVideoDisc.java
+++ src/DigitalVideoDisc.java
@@ -1,64 +1,102 @@
-
 public class DigitalVideoDisc {
     private String title;
     private String category;
     private String director;
     private int length;
     private float cost;
-    
+    private static int nbDigitalVideoDiscs = 0; // Biến đếm số DVD được tạo ra
+
+    private int id;
+
+    // Constructor chỉ có tiêu đề
     public DigitalVideoDisc(String title) {
-    	this.title = title;
+        this.title = title;
+        nbDigitalVideoDiscs++; 
+        this.id = nbDigitalVideoDiscs; 
     }
+
+    // Constructor với tiêu đề, thể loại và chi phí
     public DigitalVideoDisc(String title, String category, float cost) {
         this.title = title;
         this.category = category;
         this.cost = cost;
-}
+        nbDigitalVideoDiscs++;
+        this.id = nbDigitalVideoDiscs;
+    }
+
+    // Constructor với tiêu đề, thể loại, đạo diễn và chi phí
     public DigitalVideoDisc(String title, String category, String director, float cost) {
         this.title = title;
         this.category = category;
         this.director = director;
         this.cost = cost;
+        nbDigitalVideoDiscs++;
+        this.id = nbDigitalVideoDiscs;
     }
 
+    // Constructor đầy đủ với tất cả các thông tin
     public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
         this.title = title;
         this.category = category;
         this.director = director;
         this.length = length;
         this.cost = cost;
+        nbDigitalVideoDiscs++;
+        this.id = nbDigitalVideoDiscs;
     }
+
+    // Phương thức trả về thông tin DVD dưới dạng chuỗi
     public String toString() {
         return "Title: " + title + ", Category: " + category + ", Director: " + director + ", Length: " + length + ", Cost: $" + cost;
     }
+
+    // Các getter và setter
+    public int getId() {
+        return id;
+    }
+
     public String getTitle() {
         return title;
     }
+
     public String getCategory() {
         return category;
     }
+
     public String getDirector() {
         return director;
     }
+
     public int getLength() {
         return length;
     }
-     public float getCost() {
+
+    public float getCost() {
         return cost;
     }
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	public void setCategory(String category) {
-		this.category = category;
-	}
-	public void setDirector(String director) {
-		this.director = director;
-	}
-	public void setLength(int length) {
-		this.length = length;
-	}
-	public void setCost(float cost) {
-		this.cost = cost;
-	}
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public void setCost(float cost) {
+        this.cost = cost;
+    }
+
+    // Phương thức tĩnh trả về số lượng DVD đã tạo
+    public static int getNbDigitalVideoDiscs() {
+        return nbDigitalVideoDiscs;
+    }
 }
