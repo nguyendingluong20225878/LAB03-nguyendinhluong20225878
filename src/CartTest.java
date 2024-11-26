ecaa3e62ae63d0e904bac4ecaefd0f1f5ea8c388
nguyendinhluong20225878 <luong09876789@gmail.com>
2024-11-26 23:18:37 +0700

    CartTest

--- src/CartTest.java
+++ src/CartTest.java
@@ -3,9 +3,9 @@ public class CartTest {
         Cart cart = new Cart();
 
         // Create sample DVDs
-        DigitalVideoDisc dvd1 = new DigitalVideoDisc("Animation", "The Lion King", "Roger Allers", 87, 19.95);
-        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Science Fiction", "Star Wars", "George Lucas", 124, 24.95);
-        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Animation", "Aladdin", "Ron Clements", 90, 18.99);
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("Animation", "The Lion King", "Roger Allers", 87, 19);
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Science Fiction", "Star Wars", "George Lucas", 124, 24);
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Animation", "Aladdin", "Ron Clements", 90, 18);
 
         // Add DVDs to the cart
         cart.addDigitalVideoDisc(dvd1);
@@ -18,10 +18,8 @@ public class CartTest {
         // Test search by ID
         System.out.println("\nSearching for DVD with ID 1:");
         cart.searchById(1);
-        
         System.out.println("\nSearching for DVD with ID 3:");
         cart.searchById(3);
-
         System.out.println("\nSearching for DVD with ID 5:");
         cart.searchById(5);
 
